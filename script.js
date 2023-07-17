ac286247acd766de64fd5336c404918c1e9787d5
tolamirzayev-077 <mirodiltolamirzayev5@gmail.com>
2023-07-17 16:34:58 +0500

    Final commit

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 
 const elOver = document.querySelector('.js-overlay')
-
 setTimeout(() => {
     elOver.classList.add('hidden')
     document.body.classList.remove('y')
 }, 5000);
 
+const elBtn = document.querySelector('.js-btn')
+const elMeniu = document.querySelector('.js-meniu')
+const elExit = document.querySelector('.js-exit')
+
+
+elBtn.addEventListener('click', function () {
+    elMeniu.classList.remove('hidden')
+})
+elExit.addEventListener('click', function () {
+    elMeniu.classList.add('hidden')
+})
+
+
+//----------------------- Swiper
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
     centeredSlides: true,
     spaceBetween: 30,
     grabCursor: true,
-    
-scrollbar: {
-    el: ".swiper-scrollbar",
+
+    scrollbar: {
+        el: ".swiper-scrollbar",
         hide: true,
     },
     navigation: {
@@ -21,16 +34,26 @@ scrollbar: {
         prevEl: ".swiper-button-prev",
     },
 
+    breakpoints: {
+        340: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        540: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        640: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
 
+    },
 
     autoplay: {
         delay: 2500,
         disableOnInteraction: false,
     },
-    // pagination: {
-    //     el: ".swiper-scrollbar",
-    //     type: "progressbar",
-    //     clickable: true
-    // },
+
     loop: true
 });
